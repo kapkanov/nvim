470d617030592e5ae998ba9958f4695e278ea7ff
Anatoly Kapkanov <anatoly@kapkanov.com>
2025-12-18 09:30:20 +0300

    lazy->native package manager, tidy number of plugins

--- init.lua
+++ init.lua
@@ -16,7 +16,6 @@ vim.o.relativenumber = true
 vim.o.softtabstop    = 2
 vim.o.shiftwidth     = 2
 vim.o.expandtab      = true
--- vim.o.noexpandtab    = true
 
 -- Reserve a space in the gutter
 -- This will avoid an annoying layout shift in the screen
@@ -29,13 +28,6 @@ vim.o.smartcase      = true
 -- Scroll keeping a cursor at the center of the screen
 vim.keymap.set("n", "<c-u>", "<c-u>zz")
 vim.keymap.set("n", "<c-d>", "<c-d>zz")
-
--- Do not copy text deleted via x
-vim.keymap.set({"n", "v"}, "x", '"_x')
-
--- Move selected rows
-vim.keymap.set("v", "J", ":m '>+1<cr>gv")
-vim.keymap.set("v", "K", ":m '<-2<cr>gv")
 
 -- Keep search at center of the screen
 vim.keymap.set("n", "n", "nzz")
@@ -60,7 +52,9 @@ vim.api.nvim_create_autocmd({ "TermOpen" }, {
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", {
+  clear = true
+})
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
     vim.highlight.on_yank()
@@ -69,239 +63,103 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
 })
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
+require("onedark").setup({ style = "warm" })
+require("onedark").load()
 
-require("lazy").setup({
-	{
-		"navarasu/onedark.nvim",
-		config = function()
-			require("onedark").setup({
-				style = "warm",
-			})
-			require("onedark").load()
-		end,
-	},
+require("telescope").setup()
 
-	{
-	"nvim-treesitter/nvim-treesitter",
-	build = function()
-	  vim.cmd(":TSUpdate")
-	end,
-	config = function()
-	  local ensure = {
-	    "c",
-	    "lua",
-	    "bash",
-	    "html",
-	    "css",
-	    "dockerfile",
-	    "go",
-	    "javascript",
-	    "json",
-	    "lua",
-	    "make",
-	    "nginx",
-	    "php",
-	    "python",
-	    "sql",
-	    "typescript",
-	    "yaml",
-	  }
-          local list          = vim.api.nvim_exec("TSInstallInfo", true)
-          local ensure_result = {}
-          local languages     = {}
-          
-          for w in list:gmatch("[^\n]+") do
-            table.insert(languages, w:match("^(%S+)"))
-          end
-          
-          for _, check in ipairs(ensure) do
-            for _, lang in ipairs(languages) do
-              if lang == check then
-                table.insert(ensure_result, lang)
-              end
-            end
-          end
-          
-	  require("nvim-treesitter.configs").setup{
-	  ensure_installed = ensure_result,
-	    sync_install = false,
-
-	    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-	    auto_install = false,
-	    highlight = {enable = true},
-	  }
-	 end
-        },
-
-  { 'neovim/nvim-lspconfig' }, -- Collection of configurations for built-in LSP client
-  { 'hrsh7th/nvim-cmp' }, -- Autocompletion plugin
-  { 'hrsh7th/cmp-nvim-lsp' }, -- LSP source for nvim-cmp
-  { 'saadparwaiz1/cmp_luasnip' }, -- Snippets source for nvim-cmp
-  { 'L3MON4D3/LuaSnip' }, -- Snippets plugin
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>lg', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>gs', builtin.grep_string, { desc = 'Searches for the string under your cursor or selection in your current working directory' })
-      vim.keymap.set('n', '<leader>bu', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<leader>ht', builtin.help_tags, { desc = 'Telescope help tags' })
-      vim.keymap.set('n', '<leader>lr', builtin.lsp_references, { desc = 'Lists LSP references for word under the cursor' })
-    end,
-  },
-}, {})
-
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-  pattern = {"*.tm"},
-  callback = function(_)
-    vim.api.nvim_exec("set ft=terramate", true)
-  end
+vim.keymap.set('n', '<leader>ff', require("telescope.builtin").find_files, {
+  desc = 'Telescope find files'
 })
 
--- Add additional capabilities supported by nvim-cmp
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
+vim.keymap.set('n', '<leader>lg', require("telescope.builtin").live_grep, {
+  desc = 'Telescope live grep'
+})
 
-local lspconfig = require('lspconfig')
+vim.keymap.set('n', '<leader>gs', require("telescope.builtin").grep_string, {
+  desc = 'Searches for the string under your cursor or selection in your current working directory'
+})
 
--- Enable some language servers with the additional completion capabilities offered by nvim-cmp
--- local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver' }
-local servers = {
-  {
-    "ts_ls",
-    dependencies = { "tsc", "typescript-language-server" }
+vim.keymap.set('n', '<leader>bu', require("telescope.builtin").buffers, {
+  desc = 'Telescope buffers'
+})
+
+vim.keymap.set('n', '<leader>ht', require("telescope.builtin").help_tags, {
+  desc = 'Telescope help tags'
+})
+
+vim.keymap.set('n', '<leader>lr', require("telescope.builtin").lsp_references, {
+  desc = 'Lists LSP references for word under the cursor'
+})
+
+require("oil").setup({
+  win_options = {
+    signcolumn = "yes",
+    cursorcolumn = false,
   },
-  "pyright",
-  "gopls",
-  "jdtls",
-  "clangd",
+  skip_confirm_for_simple_edits = true,
+})
+
+vim.keymap.set('n', '<leader>o', ":Oil<CR>")
+vim.keymap.set('n', '<leader>.', require("oil").toggle_hidden, {})
+
+vim.lsp.enable("ada_ls")
+
+require("mini.completion").setup()
+
+local map_opts = {
+  expr             = true,
+  replace_keycodes = true,
+  noremap          = true,
+  silent           = true
 }
 
-if vim.fn.executable("terramate-ls") ~= 0 then
-  local configs = require("lspconfig.configs")
-  configs.terramate_ls = {
-    default_config = {
-      cmd = { "terramate-ls" },
-      filetypes = { "terramate" },
-      root_dir = function(fname)
-        return lspconfig.util.find_git_ancestor(fname) or vim.fn.getcwd()
-      end
-    }
-  }
-  table.insert(servers, {"terramate_ls", dependencies = {"terramate-ls"}})
-end
-
-for _, lsp in ipairs(servers) do
-  local lsname       = lsp[1] or lsp
-  local dependencies = lsp.dependencies or { lsp }
-  local installed    = true
-
-  for _, dependency in ipairs(dependencies) do
-    dependency = dependency[1] or dependency
-    installed  = installed and vim.fn.executable(dependency) ~= 0
-  end
-
-  if installed then
-    lspconfig[lsname].setup {
-      capabilities = capabilities,
-    }
-  end
-end
-
--- luasnip setup
-local luasnip = require 'luasnip'
-
--- nvim-cmp setup
-local cmp = require 'cmp'
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
-    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
-    -- C-b (back) C-f (forward) for snippet placeholder navigation.
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Esc>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.abort()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<C-J>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<C-K>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  }),
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  },
-}
-
-vim.api.nvim_create_autocmd("TextChangedI", {
-  pattern = "*",
-  callback = function()
-    local line = vim.api.nvim_get_current_line()
-    if line:sub(-1) == "(" then
-      vim.lsp.buf.signature_help()
+vim.keymap.set('i', '<CR>', function()
+  if vim.fn.pumvisible() ~= 0 then
+    local item_selected = vim.fn.complete_info()['selected'] ~= -1
+    
+    if item_selected then
+      return '<C-y>'
+    else
+      return '<C-n><C-y>'
     end
+  else
+    return '<CR>'
   end
-})
+end, map_opts)
 
-vim.diagnostic.config({
-  virtual_text     = true,
-  signs            = true,
-  update_in_insert = false,
-  underline        = true,
-  severity_sort    = false,
-  float            = true,
-})
+vim.keymap.set('i', '<C-K>', function()
+  if vim.fn.pumvisible() ~= 0 then
+    return "<C-p>"
+  else
+    return "<C-K>"
+  end
+end, map_opts)
 
-vim.keymap.set("n", "<leader>er", vim.diagnostic.open_float)
+vim.keymap.set('i', '<C-J>', function()
+  if vim.fn.pumvisible() ~= 0 then
+    return "<C-n>"
+  else
+    return "<C-J>"
+  end
+end, map_opts)
 
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "java",
+  pattern  = "*",
   callback = function()
-    vim.bo.shiftwidth = 4    -- Number of spaces for each indentation level
-    vim.bo.tabstop = 4       -- Number of spaces that a <Tab> in the file counts for
-    vim.bo.softtabstop = 4   -- Number of spaces a <Tab> counts for while editing
-    vim.bo.expandtab = true  -- Use spaces instead of tabs
+    local leader = vim.g.mapleader or ' '
+    
+    local all_maps = vim.list_extend(
+      vim.api.nvim_get_keymap('i'),
+      vim.api.nvim_buf_get_keymap(0, 'i')
+    )
+    
+    for _, map in ipairs(all_maps) do
+      if vim.startswith(map.lhs, leader) then
+        pcall(vim.keymap.del, 'i', map.lhs)
+        pcall(vim.keymap.del, 'i', map.lhs, { buffer = 0 })
+      end
+    end
   end,
 })
 
